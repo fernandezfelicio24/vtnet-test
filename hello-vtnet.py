638e7f982e2eb7427893e398a6208f3c9add1627
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2025-06-18 15:02:47 +0700

    make branch for this function(hello_world_function)

--- hello-vtnet.py
+++ hello-vtnet.py
@@ -1,1 +1,5 @@
-print("hello-vtnet")
+def hello_world_function():  # Fixed function name typo and added colon
+    print("WELCOME")
+    print("This is hello world function")
+
+hello_world_function()  # Changed main() to call the defined function
